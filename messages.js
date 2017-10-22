387b432dd7815a8356b76df8f73c1997053cddca
Mukul <mukul1904@gmail.com>
2017-10-22 22:59:28 +0530

    Message changed

--- messages.js
+++ messages.js
@@ -3,7 +3,7 @@
 module.exports = {
     'titleMessage': 'Welcome to NodeXperts Alexa skill!',
     'greetingMessage': 'Welcome to the NodeXperts Alexa Skill. ' +
-        'Ask any question related to NodeXperts, and you will get your answer. For demo questions, say \'demo questions\'',
+        'Ask any question related to NodeXperts, and you will get your answer. Say, \'demo questions\', for some sample questions you can ask.',
     'repromptMessage': 'Sorry I\'m not able to hear your request. Please repeat!',
     'goodByeMessgae': 'Thank you were using NodeXperts Alexa skill. We update our info regularly, do come back for latest information.'
 }
